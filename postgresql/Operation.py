c276eb0fb837f426dbf1b326a6ace6194adbca77
MathEngineer <duanyuping@outlook.com>
2017-03-17 23:46:24 +0800

    analysis html for currency data

--- postgresql/Operation.py
+++ postgresql/Operation.py
@@ -22,70 +22,103 @@ import psycopg2
 
 class Operation(object):
     def __init__(self, tableName):
-        self.conn = psycopg2.connect(database="postgres", user="postgres", password="123456", host="127.0.0.1", port="5432")
-        self.cur = self.conn.cursor()
+        self.connection = psycopg2.connect(database="postgres", user="postgres", password="123456", host="127.0.0.1", port="5432")
 
         self.tableName = tableName
 
-    def addToCurrencyTrade(self, tradeid, tradedate, currencypair, tradetype, tradeprice, tradevolume, stopprice, profitprice):
-        '''
-        往currencytrade数据库中插入数据
-        :param tradeid:
-        :param tradedate:
-        :param currencypair:
-        :param tradetype:
-        :param tradeprice:
-        :param tradevolume:
-        :param stopprice:
-        :param profitprice:
-        :return:
-        '''
-        self.cur.execute("INSERT INTO %s (  \
-                    tradeid, tradedate, currencypair, tradetype, tradeprice, tradevolume, stopprice, profitprice)  \
-                    VALUES (%s, '%s', '%s', %s, %s, %s, %s, %s)" \
-                    %(self.tableName, tradeid, tradedate, currencypair, tradetype, tradeprice, tradevolume, stopprice, profitprice ))
+    # def addToCurrencyTrade(self, tradeid, tradedate, currencypair, tradetype, tradeprice, tradevolume, stopprice, profitprice):
+    #     '''
+    #     往currencytrade数据库中插入数据
+    #     :param tradeid:
+    #     :param tradedate:
+    #     :param currencypair:
+    #     :param tradetype:
+    #     :param tradeprice:
+    #     :param tradevolume:
+    #     :param stopprice:
+    #     :param profitprice:
+    #     :return:
+    #     '''
+    #     self.cur.execute("INSERT INTO %s (  \
+    #                 tradeid, tradedate, currencypair, tradetype, tradeprice, tradevolume, stopprice, profitprice)  \
+    #                 VALUES (%s, '%s', '%s', %s, %s, %s, %s, %s)" \
+    #                 %(self.tableName, tradeid, tradedate, currencypair, tradetype, tradeprice, tradevolume, stopprice, profitprice ))
+    #
+    #     self.conn.commit()
+    #     self.cur.close()
 
-        self.conn.commit()
-        self.cur.close()
+    # def addToCurrencyTrace(self, tradeid, endprice, endprofit, enddate, highprice, highdate, lowprice, lowdate, agent):
+    #     '''
+    #     往currencytrace数据库中插入数据
+    #     :param tradeid:
+    #     :param endprice:
+    #     :param endprofit:
+    #     :param enddate:
+    #     :param highprice:
+    #     :param highdate:
+    #     :param lowprice:
+    #     :param lowdate:
+    #     :return:
+    #     '''
+    #     self.cur.execute("INSERT INTO %s (  \
+    #                     tradeid, endprice, endprofit, enddate, highprice, highdate, lowprice, lowdate, agent)  \
+    #                     VALUES (%s, %s, %s, '%s', %s, '%s', %s, '%s', '%s')" \
+    #                          % (self.tableName, tradeid, endprice, endprofit, enddate, highprice, highdate, lowprice, lowdate, agent))
+    #
+    #     self.conn.commit()
+    #     self.cur.close()
 
-    def addToCurrencyTrace(self, tradeid, endprice, endprofit, enddate, highprice, highdate, lowprice, lowdate, agent):
-        '''
-        往currencytrace数据库中插入数据
-        :param tradeid:
-        :param endprice:
-        :param endprofit:
-        :param enddate:
-        :param highprice:
-        :param highdate:
-        :param lowprice:
-        :param lowdate:
-        :return:
-        '''
-        self.cur.execute("INSERT INTO %s (  \
-                        tradeid, endprice, endprofit, enddate, highprice, highdate, lowprice, lowdate, agent)  \
-                        VALUES (%s, %s, %s, '%s', %s, '%s', %s, '%s', '%s')" \
-                             % (self.tableName, tradeid, endprice, endprofit, enddate, highprice, highdate, lowprice, lowdate, agent))
 
-        self.conn.commit()
-        self.cur.close()
+    def addToCurrencyTrades(self,  agent,  ticket , opentime, type , size , item ,  price , sl ,
+                            tp ,  closetime ,  closeprice ,  commission ,  taxes ,  swap ,  profit ):
+
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "INSERT INTO %s (agent,  ticket , opentime, type , size , item ,  price , sl , \
+                        tp ,  closetime ,  closeprice ,  commission ,  taxes ,  swap ,  profit)VALUES  \
+                      ('%s', '%s', '%s', '%s', %s, '%s', %s, %s, %s, '%s', %s, %s, %s, %s, %s)" \
+                      %(self.tableName, agent,  ticket , opentime, type , size , item ,  price , sl , tp ,  \
+                        closetime ,  closeprice , commission ,  taxes ,  swap ,  profit)
+                cursor.execute(sql)
+        finally:
+            self.connection.commit()
+
+    def compair(self):
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "select ticket from %s"%(self.tableName)
+                cursor.execute(sql)
+                results = cursor.fetchall()
+        finally:
+            self.connection.commit()
+
+        return results
+
 
 
 
 
 def main():
+    agent = 'xm'
+    ticket = '12345'
+    opentime = '2017-02-12 12:00:00'
+    type = 'buy'
+    size = 1.0
+    item = 'usreur'
+    price = 2.534
+    sl = 8.353
+    tp = 3.45
+    closetime = '2017-02-12 12:00:00'
+    closeprice = 4.3435
+    commission = 2.434
+    taxes = 4.2342
+    swap = 3.343
+    profit = 3.345
 
-    tradeid = '1703092103'
-    tradedate = '2017-02-03'
-    currencypair = 'ERUGBP'
-    tradetype = 0
-    tradeprice = 1.7434
-    tradevolume = 0.5
-    stopprice = 1.8343
-    profitprice = 1.86544
-
-    # instance = Operation('currencytrade')
-    # instance.addToCurrencyTrade(tradeid, tradedate, currencypair, tradetype, tradeprice, tradevolume, stopprice, profitprice)
-
+    instance = Operation('currencytrades')
+    # instance.addToCurrencyTrades(agent,  ticket , opentime, type , size , item ,  price , sl ,
+    #                         tp ,  closetime ,  closeprice ,  commission ,  taxes ,  swap ,  profit)
+    # print(instance.compair())
 
 if __name__ == '__main__':
     main()
