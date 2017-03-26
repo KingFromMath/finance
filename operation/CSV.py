ba16dc74306b6daffa2f7709e4720430a8766abc
MathEngineer <duanyuping@outlook.com>
2017-03-26 11:40:16 +0800

    modify

--- operation/CSV.py
+++ operation/CSV.py
@@ -17,11 +17,19 @@ create for:
     ┗┻┛  ┗┻┛
 """
 from readcsvtodb.MinData import  addInfo
+from web.CurrencyTrade import *
 
-'''更新5min/15min数据
+'''
+更新5min/15min/day数据
 '''
 param1 = '/home/duanyuping/Files/input/currency_min'    #文件结构：data1/xm/ERUCAD15.csv
 param2 = 'select * from minrecord'
 param3 = 'minrecord'
-addInfo(param1, param2, param3)
+# addInfo(param1, param2, param3)
+
+
+'''
+更新交易记录
+'''
+webContent("/home/duanyuping/Files/input/finance/fxcm.htm", 'currencytrades')
 
